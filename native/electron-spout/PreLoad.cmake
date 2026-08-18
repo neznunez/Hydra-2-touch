689100528f73b71c71e79ea7e8e55389fa8a222a
neznunez <113071857+neznunez@users.noreply.github.com>
2026-08-18 00:11:05 -0300

    Stop legacy preload from overriding vcpkg toolchain

--- native/electron-spout/PreLoad.cmake
+++ native/electron-spout/PreLoad.cmake
@@ -1,1 +1,4 @@
-set(CMAKE_TOOLCHAIN_FILE $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE INTERNAL "" FORCE)
+if(NOT DEFINED CMAKE_TOOLCHAIN_FILE AND DEFINED ENV{VCPKG_ROOT})
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE FILEPATH "vcpkg toolchain")
+endif()
+
